31f924ccf6960b5d150e5b03d6f572cbb8da78da
Piotr Siwiński <siwinskipiotr7@gmail.com>
2025-11-11 13:18:10 +0100

    Fix project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,31 @@
 rootProject.name = "Algorithms-and-data-structures"
+// Numbers module
 include("src:main:numbers")
 findProject(":src:main:numbers")?.name = "numbers"
+
+// Arrays module
 include("src:main:arrays")
 findProject(":src:main:arrays")?.name = "arrays"
+
+// Trees module
 include("src:main:trees")
 findProject(":src:main:trees")?.name = "trees"
 
+// Backtracking module
 include("src:main:backtracking")
 
+// Design patterns module
 include("src:main:designPatterns")
 findProject(":src:main:designPatterns")?.name = "designPatterns"
+
+// Object oriented design module
 include("src:main:object-oriented-design")
 findProject(":src:main:object-oriented-design")?.name = "object-oriented-design"
+
+// OOD -> Unix file system search module
 include("src:main:object-oriented-design:unix-file-system")
 findProject(":src:main:object-oriented-design:unix-file-system")?.name = "unix-file-system"
+
+// CSES.fi tasks
+include("src:main:cses")
+findProject(":src:main:cses")?.name = "cses"
--- src/main/arrays/src/main/java/ZigzagConversion.java
+++ src/main/arrays/src/main/java/ZigzagConversion.java
@@ -2,9 +2,7 @@ public class ZigzagConversion {
     static class Solution {
         public String convert(String s, int numRows) {
 
-            for (int i = 0; i < numRows; i++) {
-                for (int j = 0; j < )
-            }
+            return "";
         }
     }
 }
