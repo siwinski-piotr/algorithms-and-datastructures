34d9f287e5e7465fea5a879aa0e205a961f008d3
Piotr Siwiński <siwinskipiotr7@gmail.com>
2025-11-11 13:12:11 +0100

    Add missing build gradle files

---
+++ src/main/arrays/src/main/java/ZigzagConversion.java
@@ -0,0 +1,10 @@
+public class ZigzagConversion {
+    static class Solution {
+        public String convert(String s, int numRows) {
+
+            for (int i = 0; i < numRows; i++) {
+                for (int j = 0; j < )
+            }
+        }
+    }
+}
